438b99db646d40d273e6cdf58a5f73103d4e2830
coder-jinyeong <78059103+coder-jinyeong@users.noreply.github.com>
2022-02-07 12:23:10 +0900

    2022-02-07 12:20

--- java/com/bitcamp/web/controller/TempController.java
+++ java/com/bitcamp/web/controller/TempController.java
@@ -21,30 +21,54 @@ import java.util.Scanner;
 public class TempController {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
-        System.out.println("Q1. 팀별 과제를 출력하세요 : ");
         String[] arr = {"권혜민", "조현국",   "김진영",    "김한슬",    "서성민",
                         "정렬",   "해시",        "힙",     "완전탐색",    "DP",
                         "스택",  "깊이우선탐색","그래프",    "탐욕법",    "이중탐색",
                         "큐",    "너비우선탐색"};
-          String s = "";
-        for(int i = 0; i < arr.length; i++){
-            s += i + " : " + arr[i] + "\t";
-            if(( i + 1 ) % 5 == 0) s += "\n";
-        }
-        System.out.println(s + "\n");
-
-        System.out.println("Q2.  팀장이 맡은 과제만 출력하세요 . 예시 ) 김진영 , 힙, 그래프");
-        String zzang = "";
-        for(int i = 0; i < arr.length; i++ ){
-           if(arr[i].equals("김진영")){
-                for(int j = 0; j < 3; j++){
-                   zzang += arr[i] + ",";
-                   i += 5;
-                }
+        while(true){
+            System.out.print("번호 입력 : ");
+            switch (scanner.next()){
+                case "0":
+                    System.out.println("시스템종료"); return;
+                case "1":
+                    quiz1(arr);break;
+                case "2":
+                    quiz2(arr);break;
+                case "3":
+                    quiz3(arr);break;
+                case "4":
+                    quiz4(arr);break;
+                case "5":
+                    quiz5(scanner, arr);break;
+                default:
+                    System.out.println("잘못 입력 하였습니다.");
             }
         }
-        System.out.println(zzang + "\n");
+    }
 
+    private static void quiz5(Scanner scanner, String[] arr) {
+        System.out.print("Q5. 입력한 과제의 인덱스를 추출하세요\n과제를 입력하세요 :  ");
+        String sub = scanner.next();
+        int index = 0;
+        for(int i = 0; i < arr.length; i++){
+            if(arr[i].equals(sub))index = i % 5;
+        }
+        System.out.println("입력한 과제의 인덱스 : " + index);
+    }
+
+    private static void quiz4(String[] arr) {
+        System.out.println("Q4. 팀원별 과제 수를 출력하세요 예 ) 권혜민(3개), 조현국(3개), 김진영(2개)," +
+                        "김한슬(2개)" + "서성민(2개)");
+        int count = 0;
+        String countManager = "";
+        for(int i = 0; i < 5; i++){
+            count = (arr.length - (i + 1)) / 5;
+            countManager += arr[i] + "(" + count + "개), ";
+        }
+        System.out.println(countManager + "\n");
+    }
+
+    private static void quiz3(String[] arr) {
         System.out.println("Q3. 큐를 담당한 사람을 출력하세요. 예 ) 큐를 담당한 사람 : 권혜민");
         String qManager = "";
         for(int i = 0; i < arr.length; i++){
@@ -56,25 +80,31 @@ public class TempController {
             }
         }
         System.out.println(qManager + "\n");
+    }
 
-        System.out.println("Q4. 팀원별 과제 수를 출력하세요 예 ) 권혜민(3개), 조현국(3개), 김진영(2개)," +
-                        "김한슬(2개)" + "서성민(2개)");
-        int count = 0;
-        String countManager = "";
-        for(int i = 0; i < 5; i++){
-            count = (arr.length - (i + 1)) / 5;
-            countManager += arr[i] + "(" + count + "개), ";
+    private static void quiz2(String[] arr) {
+        System.out.println("Q2.  팀장이 맡은 과제만 출력하세요 . 예시 ) 김진영 , 힙, 그래프");
+        String zzang = "";
+        for(int i = 0; i < arr.length; i++ ){
+           if(arr[i].equals("김진영")){
+
+                for(int j = 0; j < 3; j++){
+                   zzang += arr[i] + ",";
+                   i += 5;
+                }
+            }
         }
-        System.out.println(countManager + "\n");
+        System.out.println(zzang + "\n");
+    }
 
-        System.out.print("Q5. 입력한 과제의 인덱스를 추출하세요\n과제를 입력하세요 :  ");
-        String sub = scanner.next();
-        int index = 0;
+    private static void quiz1(String[] arr) {
+        System.out.println("Q1. 팀별 과제를 출력하세요 : ");
+        String s = "";
         for(int i = 0; i < arr.length; i++){
-            if(arr[i].equals(sub))index = i % 5;
+            s += i + " : " + arr[i] + "\t";
+            if(( i + 1 ) % 5 == 0) s += "\n";
         }
-        System.out.println("입력한 과제의 인덱스 : " + index);
-
+        System.out.println(s + "\n");
     }
 }
 
